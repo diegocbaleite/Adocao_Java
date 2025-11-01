750c4109b273b8f936bacd1f464ef5419b6fa413
Diego Assunção <dev.diegocba@gmail.com>
2025-10-31 22:41:27 -0400

    public class MinioService { Classe Model do Animal e algumas configurações do

--- src/main/java/br/com/adocao/system/model/Animal.java
+++ src/main/java/br/com/adocao/system/model/Animal.java
@@ -1,27 +1,40 @@
 package br.com.adocao.system.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Animal")
+@Table(name = "ANIMAL")
 public class Animal {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100)
     private String nome;
-    private String especie; // cachorro, gato, etc
+
+    @Column(nullable = false, length = 50)
+    private String especie;
+
+    @Column(length = 50)
     private String raca;
+
+    @Column(nullable = false)
     private int idade;
+
+    @Column(length = 500)
     private String descricao;
 
+    @Column(name = "foto_url")
+    private String fotoUrl;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private StatusAnimal status = StatusAnimal.DISPONIVEL;
 }
-
---
+++ src/main/java/br/com/adocao/system/service/MinioService.java
@@ -0,0 +1,66 @@
+package br.com.adocao.system.service;
+
+import io.minio.*;
+import io.minio.http.Method;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket}")
+    private String bucketName;
+
+    public MinioService(
+            @Value("${minio.url}") String url,
+            @Value("${minio.access-key}") String accessKey,
+            @Value("${minio.secret-key}") String secretKey) {
+        this.minioClient = MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    public String uploadFile(MultipartFile file) throws Exception {
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+        try (InputStream inputStream = file.getInputStream()) {
+            // Cria o bucket se não existir
+            boolean exists = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!exists) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+
+            // Envia o arquivo
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(fileName)
+                            .stream(inputStream, file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+        }
+
+        return fileName;
+    }
+
+    public String getTemporaryUrl(String fileName) throws Exception {
+        return minioClient.getPresignedObjectUrl(
+                GetPresignedObjectUrlArgs.builder()
+                        .method(Method.GET)
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .expiry(5, TimeUnit.MINUTES) // link válido por 5 minutos
+                        .build()
+        );
+    }
+}
+
